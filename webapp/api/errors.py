4da717ba014866ec9debb1e15ed28343e602f22d
wijayaerick <wijaya.erick52@gmail.com>
2018-10-26 17:24:54 +0700

    implement search by username

---
+++ webapp/api/errors.py
@@ -0,0 +1,3 @@
+import json
+
+INVALID_QUERY = json.dumps({"id": "001", "message": "Query is invalid"})
--- webapp/api/views.py
+++ webapp/api/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from . import errors
 from instagram_web_api import Client
 
 # Create your views here.
@@ -11,7 +11,26 @@ def index(request):
     return HttpResponse(api.settings) # serialize byte??
 
 def search(request):
-    return HttpResponse("Search")
+    api = create_api_client(request)
+    query = request.GET.get('query', '')
+    search_type = request.GET.get('type', 'username')
+    if query == '':
+        print(errors.INVALID_QUERY)
+        return HttpResponse(content=errors.INVALID_QUERY, content_type='application/json', status=404)
+    
+    if search_type == 'tag':
+        pass # TBD
+    else: # search_type == 'username'
+        search_result = api.search(query)["users"]
+        users = [result["user"] for result in search_result]
+        del_list = ["profile_pic_id", "is_verified", "has_anonymous_profile_picture", 
+        "follower_count", "reel_auto_archive", "byline", "social_context", "search_social_context", 
+        "mutual_followers_count", "following", "outgoing_request", "profile_pic_url", "unseen_count"]
+        for user in users:
+            for key in del_list:
+                user.pop(key, None)
+        users = json.dumps(users)
+    return HttpResponse(content=users, content_type='application/json')
 
 def user(request, user_id): 
     return HttpResponse("User " + str(user_id))
