221681ad44922597203633b3c979163e6bf2b1cb
wijayaerick <wijaya.erick52@gmail.com>
2018-10-26 18:04:43 +0700

    get user feed

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from . import errors
 from instagram_web_api import Client
 
-# Create your views here.
 def index(request):
     api = create_api_client(request)
     return HttpResponse(api.settings) # serialize byte??
@@ -19,21 +18,31 @@ def search(request):
         return HttpResponse(content=errors.INVALID_QUERY, content_type='application/json', status=404)
     
     if search_type == 'tag':
-        pass # TBD
+        search_result = search_by_tag(api, query)
     else: # search_type == 'username'
-        search_result = api.search(query)["users"]
-        users = [result["user"] for result in search_result]
-        del_list = ["profile_pic_id", "is_verified", "has_anonymous_profile_picture", 
-        "follower_count", "reel_auto_archive", "byline", "social_context", "search_social_context", 
-        "mutual_followers_count", "following", "outgoing_request", "profile_pic_url", "unseen_count"]
-        for user in users:
-            for key in del_list:
-                user.pop(key, None)
-        users = json.dumps(users)
-    return HttpResponse(content=users, content_type='application/json')
+        search_result = search_by_username(api, query)
+    return HttpResponse(content=json.dumps(search_result), content_type='application/json')
+
+def search_by_tag(api, tag):
+    search_result = api.search(tag)["hashtags"]
+    return search_result
+
+def search_by_username(api, username):
+    del_list = ["profile_pic_id", "is_verified", "has_anonymous_profile_picture", 
+    "follower_count", "reel_auto_archive", "byline", "social_context", "search_social_context", 
+    "mutual_followers_count", "following", "outgoing_request", "profile_pic_url", "unseen_count"]
+
+    search_result = api.search(username)["users"]
+    users = [result["user"] for result in search_result]
+    for user in users:
+        for key in del_list:
+            user.pop(key, None)
+    return users
 
 def user(request, user_id): 
-    return HttpResponse("User " + str(user_id))
+    api = create_api_client(request)
+    user_feed = api.user_feed(user_id)
+    return HttpResponse(json.dumps(user_feed))
 
 def create_api_client(request):
     if request.COOKIES.get('iganalyzer'):
