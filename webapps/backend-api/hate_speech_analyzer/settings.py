09abd55a82c21fe623f1e04ec89001618716d0f7
wijayaerick <wijaya.erick52@gmail.com>
2018-11-04 14:23:58 +0700

    docker compose

--- webapps/backend-api/hate_speech_analyzer/settings.py
+++ webapps/backend-api/hate_speech_analyzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gi16v6pcuz_b76xv=f-b%yxyxzer1butr1vcr(6!@qo%#+@vgu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
